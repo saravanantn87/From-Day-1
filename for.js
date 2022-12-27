ce3b6e18ca40ffe74241d921d1d82794fb6ae748
saravanantn87 <111057164+saravanantn87@users.noreply.github.com>
2022-12-27 17:02:30 +0530

    Add files via upload

---
+++ for.js
@@ -0,0 +1,20 @@
+//For
+
+var arr3 = []
+arr3[100]=35;
+console.log(arr3);
+
+// For of
+var arr = [1,2,3,4,5];
+
+for (var z of arr) {
+    console.log(z)
+}
+
+//For in
+
+var arr = [1,2,3,4,5];
+
+for (var i in arr) {
+    console.log(i)
+}
